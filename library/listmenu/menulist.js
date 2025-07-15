d613b533d1cfe61aed7b35459aaeed5907d69f35
Tennor-modz <giddysarry@gmail.com>
2025-07-15 23:21:25 +0300

    Add files via upload

---
+++ library/listmenu/menulist.js
@@ -0,0 +1,26 @@
+const chalk = require('chalk')
+const fs = require('fs')
+const aiMenu = `╭─⊷𝐓𝐑𝐀𝐒𝐇𝐂𝐎𝐑𝐄─
+│▢ Owner:${global.ownername}
+│▢ Version: 1.2.0
+│▢ Type: ${global.typebot}
+╰────────────
+╭─⊷🐦‍🔥MAIN-CMD─
+│ • menu                    
+│ • bot
+│ • listplugin        
+╰────────────
+╭─⊷🗿CTRL-BOT─
+│ • public                    
+│ • private 
+│ • getplugin        
+╰────────────`
+
+module.exports = aiMenu
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
