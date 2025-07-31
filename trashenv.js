7eabd2f57257d60c01b3d76c0f69d98057b3581d
Tennor-modz <giddysarry@gmail.com>
2025-07-31 20:45:56 +0300

    Update trashenv.js

--- trashenv.js
+++ trashenv.js
@@ -6,7 +6,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 global.SESSION_ID = process.env.SESSION_ID || '' 
 // Owner Setting
 global.xprefix = process.env.BOT_PREFIX ||'.'
-global.owner = ["254104245659",]
+global.owner = ["254703726139",]
 global.error = ["6666",]
 global.ownername = process.env.OWNER_NAME ||'Trashcore'
 global.antidelete = process.env.ANTI_DELETE || true
